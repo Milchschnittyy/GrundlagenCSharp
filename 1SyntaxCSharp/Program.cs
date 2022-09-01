d13fb26c6d321b7161f32e57673e5f21a54d29f2
René Pruchniewicz <rene.p8@gso.schule.koeln>
2022-09-01 10:18:14 +0200

    ud

--- 1SyntaxCSharp/Program.cs
+++ 1SyntaxCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("test");
-//TEST
-
-//test2
+Console.WriteLine("Welcome,Milchschnitte");
+int rene_alter;
+rene_alter = 17;
+Console.WriteLine("Rene ist {0} Jahre alt");
+Console.WriteLine("$");
